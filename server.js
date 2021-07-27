e3db55d92cabdcad6ecca9699fae3b339bd2faf7
luandamato <luandamato@gmail.com>
2021-07-27 20:50:36 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const SocketServer = require('websocket').server
+const http = require('http')
+
+const server = http.createServer((req, res) => {})
+
+server.listen(3000, ()=>{
+    console.log("rodando no porta 3000")
+})
+
+wsServer = new SocketServer({httpServer:server})
+
+const connections = []
+
+wsServer.on('request', (req) => {
+    const connection = req.accept()
+    console.log('nova conexão')
+    connections.push(connection)
+
+    connection.on('message', (mes) => {
+        connections.forEach(element => {
+            if (element != connection)
+                element.sendUTF(mes.utf8Data)
+        })
+    })
+
+    connection.on('close', (resCode, des) => {
+        console.log('connection closed')
+        connections.splice(connections.indexOf(connection), 1)
+    })
+
+})
---
+++ teste.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html')
+
+app.use('/web', (req, res) => {
+    res.render('index.html');
+})
+
+let mensagens =  []
+let conexoes = []
+
+io.on('connection', socket =>{
+    console.log('nova conexão: '+socket.id)
+
+    socket.emit('mensagensAnteriores', mensagens);
+    conexoes.push(socket.id);
+
+    socket.on('enviarMensagem', data =>{
+        mensagens.push(data);
+        socket.broadcast.emit('mensagemRecebida', data);
+    })
+
+    socket.on('disconnect', function() {
+  
+        var i = conexoes.indexOf(socket.id);
+        conexoes.splice(i, 1);
+     });
+})
+
+server.listen(3000, ()=> {
+    console.log("rodando no porta 3000")
+})
